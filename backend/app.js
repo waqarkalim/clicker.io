8a315e6cf46f2cd77d913c36f01dbbcb4ca26992
waqar.kalim <waqaar.199@gmail.com>
2021-02-11 01:48:26 -0500

    Changed backend deployement command again

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'))
+})
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
